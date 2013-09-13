37c9ffd6fad45284e7a6dc69a9eb830a0c895841
Klemen Košir <klemenkosir@Klemens-Mac.local>
2013-09-13 19:12:01 +0200

    test1

--- MovingSpaceBoat/MovingSpaceBoat/MyScene.m
+++ MovingSpaceBoat/MovingSpaceBoat/MyScene.m
@@ -27,6 +27,7 @@
         [spaceboat setPosition:CGPointMake(self.size.width/2, self.size.height/2)];
         spaceboat.anchorPoint = CGPointMake(0.5f, 0.5f);
         [self addChild:spaceboat];
+        
     }
     return self;
 }
